21b379afaf45744c6320cf861d37963495c316b0
dahlia <wlstnrdahlia@gmail.com>
2023-01-08 16:04:33 +0900

    feat : tabmenu js 코드 추가, window scrol 시 애니메이션 js 코드 추가

--- 류진숙/toy-project week3/static/js/script.js
+++ 류진숙/toy-project week3/static/js/script.js
@@ -1,4 +1,28 @@
 
+// header 부분 tab 클릭 시 style 이벤트
+try {
+
+  let clickedIndex;
+  const tabMenu = document.querySelectorAll('.tabmenu > a');
+
+  for (let i=0; i < tabMenu.length; i++) {
+
+    tabMenu[i].addEventListener('click', function() {
+      tabMenu[i].classList.add('checked');
+
+      clickedIndex = i;
+
+      tabMenu.forEach(function(menu, index) {
+        if (index != clickedIndex) {
+          menu.classList.remove('checked');
+        }
+      });
+
+    });
+  } 
+} catch { }
+
+
 // Q & A dropdown toggle 구현
 // 제주도 질문 부분 잘안눌림 - 추후 해결 할 것
 try {
@@ -21,17 +45,30 @@ try {
 
 } catch { }
 
+
+
 // 스크롤 할때 title 나타나게끔 하기
-// try {
-//   const sectionTwoTitle = document.querySelector('.section__bg-img2--title-area');
-//   const sectionFiveTitle = document.querySelector('.section__bg-img5--title-area');
-//   window.addEventListener('scroll', () => {
-//     sectionTwoTitle.style.transition = "0.5s ease";
-//     sectionTwoTitle.style.opacity = "1";
-//     sectionFiveTitle.style.transition = "0.5s ease";
-//     sectionFiveTitle.style.opacity = "1";
-//   });
-// } catch {}
+try {
+  const sectionTwoTitle = document.querySelector('.section__bg-img2--title-area');
+  scrollTwoTitleY = sectionTwoTitle.getBoundingClientRect().top;
+  const sectionFiveTitle = document.querySelector('.section__bg-img5--title-area');
+  scrollFiveTitleY = sectionFiveTitle.getBoundingClientRect().top;
+  
+  console.log(scrollTwoTitleY);
+  console.log(scrollFiveTitleY);
+  window.addEventListener('scroll', () => {
+    if (window.scrollY <= scrollTwoTitleY) {
+      sectionTwoTitle.style.transition = "1s ease";
+      sectionTwoTitle.style.opacity = "1";
+    }
+    if ( window.scrollY + 1000 > scrollFiveTitleY) { 
+      sectionFiveTitle.style.transition = "0.5s ease";
+      sectionFiveTitle.style.opacity = "1";
+    } // 조금 억지스럽게 맞춘건 있지만 성공,,
+  });
+ } catch {}
+
+
 
 // top 버튼 부드럽게 스크롤해서 올라가게 만들기
 try {
